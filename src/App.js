86dfd1106d660552815442df75c0bc71b5ec7e76
skifreetony <skifreetony@yahoo.com>
2021-02-19 21:06:10 -0800

    Added cleanup to city history fetch

--- src/App.js
+++ src/App.js
@@ -34,28 +34,31 @@ function App() {
         <Header />   
           <LastUpdateDate.Provider value= {{lastDate,setDate}}>
           <TimeContext.Provider value={ { time, setTime } }>
+            <React.StrictMode>
           <Switch>
-              <Route exact path='/' component={ Home } />
-              <Route path='/cases' component={ Cases } />
-              <Route path='/deaths' component={ Deaths } />
-              <Route path='/hospitalizations' component={ Hospitalization }  />
-              <Route path='/vaccinations' component={ Vaccinations } />
-              <Route path='/testing' component={ Testing } />
-              <Route path='/serology' component={ Serology } />
-              <Route path='/schools' component={Schools}/>
-              <Route path='/cityhistory' component={Cityhistory }/>
-              <Route path='/city' component={ City} />
-              <Route path='/zip' component={ Zip } />
-              <Route path='/age' component={Age } />
-              <Route path='/race' component={Race} />
-              <Route path='/gender' component={ Gender }/>
-              <Route path='/whatsopen' component={ WhatsOpen} />
-              <Route path='/reportbugs' component={ Reportbug } />
-              <Route path='/donate' component={ Donate } />
-              <Route path='/compare' component={ Compare} />
-              <Route component={ NoPage} />
+         
+                <Route exact path='/' component={ Home } />
+                <Route path='/cases' component={ Cases } />
+                <Route path='/deaths' component={ Deaths } />
+                <Route path='/hospitalizations' component={ Hospitalization } />
+                <Route path='/vaccinations' component={ Vaccinations } />
+                <Route path='/testing' component={ Testing } />
+                <Route path='/serology' component={ Serology } />
+                <Route path='/schools' component={ Schools } />
+                <Route path='/cityhistory' component={ Cityhistory } />
+                <Route path='/city' component={ City } />
+                <Route path='/zip' component={ Zip } />
+                <Route path='/age' component={ Age } />
+                <Route path='/race' component={ Race } />
+                <Route path='/gender' component={ Gender } />
+                <Route path='/whatsopen' component={ WhatsOpen } />
+                <Route path='/reportbugs' component={ Reportbug } />
+                <Route path='/donate' component={ Donate } />
+                <Route path='/compare' component={ Compare } />
+                <Route component={ NoPage } />
+            
           </Switch>
-           
+             </React.StrictMode>
           </TimeContext.Provider>
           </LastUpdateDate.Provider>   
       </div>
--- src/Datafetch/FetchCityHistory.jsx
+++ src/Datafetch/FetchCityHistory.jsx
@@ -4,63 +4,62 @@ import moment from 'moment'
 
 
 const FetchCityHistory = (props) => {
-    //let thisDataArray = [];
-    //let fetchCity1 = `https://opendata.arcgis.com/datasets/772f5cdbb99c4f6689ed1460c26f4b05_0/FeatureServer/0/query?where=1%3D1&outFields=DateSpecCollect,${props.cities[0]},&returnGeometry=false&orderByFields=OBJECTID ASC&outSR=&f=json`;
-    const [update1, update2, update3] = props.arrayUpdate
+    //const [update1, update2, update3] = props.arrayUpdate
     const [city1, city2, city3] = props.cities;
 
-
-    async function fetchArray(cityName, i) {
-        let cityData = [];
-        let fetchURL = `https://opendata.arcgis.com/datasets/772f5cdbb99c4f6689ed1460c26f4b05_0/FeatureServer/0/query?where=1%3D1&outFields=DateSpecCollect,${cityName},&returnGeometry=false&orderByFields=OBJECTID ASC&outSR=&f=json`;
-        await fetch(fetchURL)
-            .then(a => a.json())
-            .then(b => b.features.map(c => c.attributes))
-            .then(d => {
-                d.forEach(d => {
-                    let date = new Date((Object.values(d)[0]));
-                    let shortDate = moment(date).format('l')
-                    if (shortDate !== 'Invalid date') {
-                        cityData.push({
-                            date: shortDate,
-                            cases: Object.values(d)[1]
+    return <>
+        {
+            useEffect(() => {
+                let mounted = true;
+                const getData = async (cityName, i) => {
+                    let cityData = [];
+                    let fetchURL = `https://opendata.arcgis.com/datasets/772f5cdbb99c4f6689ed1460c26f4b05_0/FeatureServer/0/query?where=1%3D1&outFields=DateSpecCollect,${cityName},&returnGeometry=false&orderByFields=OBJECTID ASC&outSR=&f=json`;
+                    await fetch(fetchURL)
+                        .then(a => a.json())
+                        .then(b => b.features.map(c => c.attributes))
+                        .then(d => {
+                            d.forEach(d => {
+                                let date = new Date((Object.values(d)[0]));
+                                let shortDate = moment(date).format('l')
+                                if (shortDate !== 'Invalid date') {
+                                    cityData.push({
+                                        date: shortDate,
+                                        cases: Object.values(d)[1]
+                                    })
+                                }
+                            })
                         })
+                        .then(() => {
+                            return filtertime(cityData, props.time);
+                        }).then((final) => {
+                            if (mounted) {
+                                props.arrayUpdate[i](final)
+                            }
+                        })
+                }
+                if (mounted) {
+                    try {
+                        if (city1) {
+                            getData(city1, 0);
+                        }
+                        if (city2) {
+                            getData(city2, 1);
+                        }
+                        if (city3) {
+                            getData(city3, 2);
+                        }
+                    } catch (err) {
+                        console.log("Could not fetch data for city")
+                        console.log(err)
                     }
-
-                })
-            })
-            .then(() => {
-                return filtertime(cityData, props.time);
-            }).then((final) => {
-                props.arrayUpdate[i](final)
-            })
-    }
-
-    useEffect( () => {
-        if (city1) {
-            fetchArray(city1, 0);
+                }
+                return () => {
+                    mounted = false;
+                }
+            }, [props.time, city1, city2, city3])
         }
-        if (city2) {
 
-            fetchArray(city2, 1);
-        }
-        if (city3) {
-
-            fetchArray(city3, 2);
-        }
-    }, [props.time])
-
-    useEffect( () => {
-        fetchArray(city1, 0);
-    }, [city1])
-    useEffect( () => {
-        fetchArray(city2, 1);
-    }, [city2])
-    useEffect( () => {
-        fetchArray(city3, 2);
-    }, [city3])
-
-    return <></>
+    </>
 }
 
 export default FetchCityHistory;
