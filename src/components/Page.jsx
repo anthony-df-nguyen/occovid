46db4f5f0b1e0ccd18497b4a4e92a160433b5afc
skifreetony <skifreetony@yahoo.com>
2021-02-22 23:05:23 -0800

    Added scroll to top button

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,30 +1,55 @@
-import React, {useEffect} from 'react';
-import Footer from 'components/Footer'
-import Lastupdate from 'components/Lastupdate'
+import React, { useEffect } from "react";
+import Footer from "components/Footer";
+import Lastupdate from "components/Lastupdate";
+import BackToTop from "components/BackToTop";
 
 const Page = (props) => {
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
-    
-    const generateUpdateDate = (x) => {
-        if (x !== 'Vaccinations' && x !== 'School Cases' && x !== 'City History') {
-            return <Lastupdate />
-        }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const generateUpdateDate = (x) => {
+    if (x !== "Vaccinations" && x !== "School Cases" && x !== "City History") {
+      return <Lastupdate />;
     }
-    document.title = props.title;
+  };
 
-    return (
-        <div>
-            <div className="page">
-                <h1 className='pageTitle'>{ props.title }</h1>
-                {generateUpdateDate(props.title)}
-                { props.children }
-                <Footer />
+  const scrollToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
 
-            </div>  
+  };
+
+  document.title = props.title;
+
+  useEffect(() => {
+    const backTopButton = document.getElementById("backToTopButton");
+    const handleScroll = () => {
+      let scrollHeight = window.scrollY;
+      if (scrollHeight > 400) {
+        backTopButton.style.display = "block";
+      } else {
+        backTopButton.style.display = "none";
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="page">
+        <h1 className="pageTitle">{props.title}</h1>
+        {generateUpdateDate(props.title)}
+        {props.children}
+        <div className="backToTop" id="backToTopButton" onClick={scrollToTop}>
+          <BackToTop id="arrow" />
         </div>
-    );
-}
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
