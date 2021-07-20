9c068bd9ffe1476bb491320395dcf94cd04459c1
skifreetony <skifreetony@yahoo.com>
2021-07-20 10:58:46 -0700

    Disable tuesday only logic for now

--- src/globalVars/Sources.jsx
+++ src/globalVars/Sources.jsx
@@ -4,34 +4,34 @@ const dayOfWeek = today.getDay()
 
 let lastTuesday = today;
 
-//If it isnt Sunday or Monday
-if (dayOfWeek !== 2 && dayOfWeek !== 0 && dayOfWeek !== 1) {
-  console.log(
-    "%cToday is Not a Sunday, Monday, or Tuesday",
-    "color: #fff; background: #009ddb;font-size:20px"
-  );
-  const delta = dayOfWeek - 2;
-  console.log(
-    `Today is ${today.toLocaleString()} which is the ${dayOfWeek} day of the week or ${delta} day(s) away from Tuesday`
-  );
-  lastTuesday = new Date(today.setDate(today.getDate() - delta));
-  console.log("The date of the last Tuesday is: ", lastTuesday);
-}
-//If a Sunday
-else if (dayOfWeek === 0) {
-  console.log("%cToday is Sunday", "color: #fff; background: #009ddb;font-size:20px");
-  lastTuesday = new Date(today.setDate(today.getDate() - 5));
-  console.log("The date of the last Tuesday is ", lastTuesday);
-} 
-//If Monday
-else if (dayOfWeek === 1) {
-  console.log(
-    "%cToday is Monday",
-    "color: #fff; background: #009ddb;font-size:20px"
-  );
-  lastTuesday = new Date(today.setDate(today.getDate() - 6));
-  console.log("The date of the last Tuesday is: ", lastTuesday);
-}
+// //If it isnt Sunday or Monday
+// if (dayOfWeek !== 2 && dayOfWeek !== 0 && dayOfWeek !== 1) {
+//   console.log(
+//     "%cToday is Not a Sunday, Monday, or Tuesday",
+//     "color: #fff; background: #009ddb;font-size:20px"
+//   );
+//   const delta = dayOfWeek - 2;
+//   console.log(
+//     `Today is ${today.toLocaleString()} which is the ${dayOfWeek} day of the week or ${delta} day(s) away from Tuesday`
+//   );
+//   lastTuesday = new Date(today.setDate(today.getDate() - delta));
+//   console.log("The date of the last Tuesday is: ", lastTuesday);
+// }
+// //If a Sunday
+// else if (dayOfWeek === 0) {
+//   console.log("%cToday is Sunday", "color: #fff; background: #009ddb;font-size:20px");
+//   lastTuesday = new Date(today.setDate(today.getDate() - 5));
+//   console.log("The date of the last Tuesday is ", lastTuesday);
+// } 
+// //If Monday
+// else if (dayOfWeek === 1) {
+//   console.log(
+//     "%cToday is Monday",
+//     "color: #fff; background: #009ddb;font-size:20px"
+//   );
+//   lastTuesday = new Date(today.setDate(today.getDate() - 6));
+//   console.log("The date of the last Tuesday is: ", lastTuesday);
+// }
 
 
 const tomorrow = new Date(lastTuesday);
